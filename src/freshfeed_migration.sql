8fe4d71d230dbce28328a837bbfd5aa2d0615d32
Joanna <m.joanna.foss@gmail.com>
2021-12-10 11:28:50 -0600

    feat: edits to sqls

--- src/freshfeed_migration.sql
+++ src/freshfeed_migration.sql
@@ -2,8 +2,7 @@ CREATE DATABASE IF NOT EXISTS thefreshfeed_db;
 
 USE thefreshfeed_db;
 
-
-CREATE TABLE IF NOT EXISTS customer (
+CREATE TABLE IF NOT EXISTS user (
     id INT UNSIGNED AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
@@ -17,8 +16,8 @@ CREATE TABLE IF NOT EXISTS customer (
 
 CREATE TABLE IF NOT EXISTS vendor (
     id INT UNSIGNED AUTO_INCREMENT NOT NULL,
-    cust_id INT UNSIGNED,
-    FOREIGN KEY (cust_id) REFERENCES customer (id),
+    user_id INT UNSIGNED,
+    FOREIGN KEY (user_id) REFERENCES user (id),
     vendor_name VARCHAR(50) NOT NULL UNIQUE,
     PRIMARY KEY (id),
 );
@@ -57,6 +56,6 @@ CREATE TABLE IF NOT EXISTS ad (
 CREATE TABLE IF NOT EXISTS favorite (
     user_id INT UNSIGNED,
     ad_id INT UNSIGNED,
-    FOREIGN KEY (cust_id) REFERENCES customer (id),
+    FOREIGN KEY (user_id) REFERENCES user (id),
     FOREIGN KEY (ad_id) REFERENCES ad (id)
 );
--- src/freshfeed_seeder.sql
+++ src/freshfeed_seeder.sql
@@ -1,21 +1,17 @@
 USE thefreshfeed_db;
-# #
-# INSERT INTO user (first_name, last_name, email, username, password, is_vendor) VALUES
-#     ('Rodriques', 'Perry', 'mrperry@email.com', 'rperry', 'password123', true),
-#     ('John', 'Doe', 'john@doe.com', 'john', 'password123', false),
-#     ('Kevin', 'Orta', 'mrorta@email.com', 'korta', 'password123', true),
-#     ('Jane', 'Doe', 'jane@doe.com', 'jane', 'password123', false),
-#     ('Joanna', 'Foss', 'msfoss@email.com', 'jfoss', 'password123', true);
-#
-# INSERT INTO vendor (user_id, vendor_name) VALUES
-#     (1, 'Perry\'s Veggie Platters'),
-#     (2, 'Orta\'s Milk Stand'),
-#     ((SELECT user.id FROM user WHERE user.first_name = 'Joanna'), 'Foss\'s Florals');
-#
-#
-# INSERT INTO ad (title, description) VALUES ('10 Bundles of Celery', '10 Bundle of Celery, Non-GMO, Freshly Sourced, Plentiful crops');
-#
+
+INSERT INTO user (first_name, last_name, email, username, password, is_vendor) VALUES
+    ('Rodriques', 'Perry', 'mrperry@email.com', 'rperry', 'password123', true),
+    ('John', 'Doe', 'john@doe.com', 'john', 'password123', false),
+    ('Kevin', 'Orta', 'mrorta@email.com', 'korta', 'password123', true),
+    ('Jane', 'Doe', 'jane@doe.com', 'jane', 'password123', false),
+    ('Joanna', 'Foss', 'msfoss@email.com', 'jfoss', 'password123', true);
+
+INSERT INTO vendor (user_id, vendor_name) VALUES
+    (1, 'Perry\'s Veggie Platters'),
+    (2, 'Orta\'s Milk Stand'),
+    ((SELECT user.id FROM user WHERE user.first_name = 'Joanna'), 'Foss\'s Florals');
+
+INSERT INTO ad (title, description) VALUES ('10 Bundles of Celery', '10 Bundle of Celery, Non-GMO, Freshly Sourced, Plentiful crops');
 INSERT INTO ad(title, description, price) VALUES ('50 ears of Corn', 'Beautiful, yellow Corn, freshly pulled from stalks', '100')
-#
-#
-# ALTER table ad ADD price int unsigned not null;
+ALTER table ad ADD price int unsigned not null;
