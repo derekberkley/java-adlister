f2e1561c7c182a1e2f05768377444dd12b6cf6b1
Joanna <m.joanna.foss@gmail.com>
2021-12-10 16:50:42 -0600

    completed: user can only create ad if signed in

--- src/main/java/com/codeup/adlister/controllers/CreateAdServlet.java
+++ src/main/java/com/codeup/adlister/controllers/CreateAdServlet.java
@@ -14,7 +14,12 @@ import java.io.IOException;
 @WebServlet(name = "controllers.CreateAdServlet", urlPatterns = "/ads/create")
 public class CreateAdServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/WEB-INF/ads/create.jsp").forward(request, response);
+        User user = (User) request.getSession().getAttribute("user");
+        if(user != null) {
+            request.getRequestDispatcher("/WEB-INF/ads/create.jsp").forward(request, response);
+        } else {
+            request.getRequestDispatcher("/login").forward(request, response);
+        }
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
