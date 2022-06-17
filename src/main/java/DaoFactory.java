0a1b56121b31f895f01846325290ffb076509c6a
Derek <99042983+derekberkley@users.noreply.github.com>
2022-06-17 16:32:36 -0500

    completed jdbc exercise

--- src/main/java/DaoFactory.java
+++ src/main/java/DaoFactory.java
@@ -1,9 +1,13 @@
 public class DaoFactory {
     private static Ads adsDao;
 
+//    TODO: When you create an instance of MySQLAdsDao, create an instance of your Config class to pass to the MySQLAdsDao constructor
+    private static Config config = new Config();
+
+    //TODO: In the DaoFactory class, switch out the ListAdsDao for your MySQLAdsDao
     public static Ads getAdsDao() {
         if (adsDao == null) {
-            adsDao = new ListAdsDao();
+            adsDao = new MySQLAdsDAO(config);
         }
         return adsDao;
     }
