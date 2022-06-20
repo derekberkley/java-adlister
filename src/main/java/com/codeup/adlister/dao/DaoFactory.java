0ea8aa7eaa1125e5be3ee819794033687706712a
Derek <99042983+derekberkley@users.noreply.github.com>
2022-06-20 12:32:44 -0500

    lunch

--- src/main/java/com/codeup/adlister/dao/DaoFactory.java
+++ src/main/java/com/codeup/adlister/dao/DaoFactory.java
@@ -1,8 +1,22 @@
 package com.codeup.adlister.dao;
 
+import com.codeup.adlister.models.User;
+
 public class DaoFactory {
     private static Ads adsDao;
     private static Config config = new Config();
+    public static Users users = new Users() {
+
+        @Override
+        public User findByUsername(String username) {
+            return null;
+        }
+
+        @Override
+        public Long insert(User user) {
+            return null;
+        }
+    };
 
     public static Ads getAdsDao() {
         if (adsDao == null) {
@@ -10,4 +24,15 @@ public class DaoFactory {
         }
         return adsDao;
     }
+    public static Ads getUsersDao() {
+        if (adsDao == null) {
+            adsDao = new MySQLAdsDao(config);
+        }
+        return adsDao;
+    }
+
+
+// TODO: Add functionality to the DaoFactory class to return an instance of the users data access object.
+
+
 }
---
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDAO.java
@@ -0,0 +1,17 @@
+package com.codeup.adlister.dao;
+
+import com.codeup.adlister.models.User;
+
+//TODO: Create a class named MySQLUsersDao that implements Users.
+public class MySQLUsersDAO implements Users{
+
+    @Override
+    public User findByUsername(String username) {
+        return null;
+    }
+
+    @Override
+    public Long insert(User user) {
+        return null;
+    }
+}
